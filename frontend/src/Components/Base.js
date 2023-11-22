44f103f1de9b2992351f30bd7698e0b2195608d4
amaljs <amaljs121@gmail.com>
2023-11-22 23:17:03 +0530

    comments added in the react components

--- frontend/src/Components/Base.js
+++ frontend/src/Components/Base.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './Base.css'
 import Task from './Task';
+// fetch data from the server
 import axios from 'axios'
 
 
 const Base = () => {
 
 
-
+    //state for storing the task objects array
     const [tasks,setTasks] = useState([])
-
-
+    //state for storing the new task text
+    const [newtask,setNewTask] = useState('')
+    // data access url
+    const url = 'http://localhost:8000/tasks/'
+    //function for the fetch data and set the array in the tasks state
     const fetchData = ()=>{
 
         axios.get(url)
+
         .then((response) => {
         
+        //passing the array
         setTasks(response.data);
-        console.log('update   ',response.data)
+       
 
       })
       .catch((err)=>{
@@ -27,9 +33,9 @@ const Base = () => {
  
       ;}
 
-    const url = 'http://localhost:8000/tasks/'
 
 
+//useEffect for the initial fetch of the data
   useEffect(() => {
     fetchData()
     
@@ -39,42 +45,56 @@ const Base = () => {
 
 
 
-    
+    //function to handle the delete the task object , gets the task id passing that id with the url with the method to delete
+
     const deleteTask =(id)=>{
         
         axios.delete(url+id)
+
         .then((res)=>{
-            console.log(res)
+            //if deletion is success fetch the data update the array state
             fetchData()
         }).catch(
             err => console.log(err)
         )
         
     }
+
+    //function to add the new task
     const addNewTask = async () => {
+
         const newData = {
-          task: document.getElementById('newTask').value,
+          task: newtask,
         };
       
+        // passing the new object with the data to the url with the axios post request
         try {
           await axios.post(url, newData);
+          //if success then update the array
+          //empty the input field
+          
+          
           fetchData();
+          setNewTask('');
         } catch (error) {
           console.log(error);
         }
       };
       
+      //update the task recives the object with update data from the task component 
       const updateTask = async (id, data) => {
         try {
           
+          //sending the data using put method along with the id of object
           const response = await axios.put(`${url}${id}/`, data);
           
       
+          //updating the array by using filter method and previous state of the tasks
           setTasks(prevTasks => {
             const updatedTasks = prevTasks.map(task =>
               task.id === id
                 ? {
-                    ...task,
+                    ...task, //destructing the except tasks
                     task: data.task !== undefined ? data.task : task.task,
                     completed: data.completed !== undefined ? data.completed : task.completed,
                   }
@@ -105,7 +125,8 @@ const Base = () => {
                 
 
                     <div className='d-flex justify-content-center m-5'>
-                        <input type='text' id='newTask' placeholder=' Add a new task' className='mx-3'></input>
+                      {/* onchange the value will be set to the newtask state */}
+                        <input type='text' id='newTask' placeholder=' Add a new task' className='mx-3' defaultValue={newtask} onChange={(evt=> setNewTask(evt.target.value))}></input>
                         <button id='addNewTaskBtn' onClick={addNewTask} className='btn btn-success'>Add</button>
                     </div>
                    </div>
@@ -115,7 +136,8 @@ const Base = () => {
                         tasks.map((element)=>{
 
                             return (
-
+                              // key = id task completed object because when key,value change then only re render else the object is same 
+                              //if you give only id or the value to the key 
                                 <Task key={`${element.id}${element.task}${element.completed}`} task={element} deleteTask={()=>deleteTask(element.id)} updateTask={updateTask}/>
                             )
                         })
--- frontend/src/Components/Task.js
+++ frontend/src/Components/Task.js
@@ -1,93 +1,131 @@
-import {React , useState }from 'react'
-import axios  from 'axios'
+import { React, useState } from "react";
 
-import './Task.css'
-import url from './Base.js'
+import "./Task.css";
 
-const Task = (props)=>{
+const Task = (props) => {
+  // for hiding input border when element not focused
+  const [focused, setFocus] = useState(false);
+  // each task object
+  const [taskObject, setTaskObject] = useState(props.task);
+  // task state
+  const [task, setTask] = useState(taskObject.task);
+  // completed state
+  const [completed, setCompleted] = useState(taskObject.completed);
 
+  // task delete function
+  const deleteTask = props.deleteTask;
+  // task update function
+  const updateTask = props.updateTask;
 
-  const [focused,setFocus] =useState(false)
-  const [taskObject,setTaskObject] =useState(props.task)
-  const [task,setTask] = useState(taskObject.task)
-  const [completed,setCompleted] = useState(taskObject.completed)
+  //focus handling function
+  const handleFocus = (event) => {
+    setFocus(true);
+  };
 
-  const deleteTask = props.deleteTask
-  const updateTask =props.updateTask
-    
-    const handleFocus =(event) =>{
-        setFocus(true)
-        
-    }
+  //focus handling function
+  const handleFocusLeave = (event) => {
+    setFocus(false);
+  };
 
-    const handleFocusLeave = (event) =>{
-        setFocus(false)
-        
-    }
-
-    const handleUpdateTask = ()=>{
-
+  //handle update action pass the task object current task text and completed status and calling the  update task in the parent componet
+  const handleUpdateTask = () => {
     const data = {
-            task :task,
-            completed:completed
-        }
+      task: task,
+      completed: completed,
+    };
 
-       
-        updateTask(taskObject.id,data)
-    }
+    updateTask(taskObject.id, data);
+  };
 
+  return (
+    <div
+      className={` p-2 mt-3 task-div ${
+        taskObject.completed ? "border-0" : "text-dark"
+      } m-2 d-flex justify-content-evenly`}
+    >
+      <div className="d-flex w-75">
+        {taskObject.completed ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="bi bi-hand-thumbs-up-fill text-success mt-2"
+            viewBox="0 0 16 16"
+          >
+            <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="bi bi-hand-thumbs-up-fill text-danger mt-2"
+            viewBox="0 0 16 16"
+          >
+            <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+          </svg>
+        )}
 
-    return( 
-        <div className={` p-2 mt-3 task-div ${taskObject.completed ? 'border-0' :'text-dark'} m-2 d-flex justify-content-evenly`} >
+        <input
+          type="checkbox"
+          id={`taskDone-${taskObject.id}`}
+          defaultChecked={completed}
+          className="mx-5"
+          onChange={(evt) => setCompleted(evt.target.value)}
+        ></input>
 
-            
+        <input
+          type="text"
+          id={`taskText-${taskObject.id}`}
+          defaultValue={task}
+          className={`input-elements  ${focused ? "focused-element" : ""} ${
+            completed ? "text-success fw-bold " : "text-dark"
+          } w-75`}
+          placeholder="default"
+          onFocus={handleFocus}
+          onBlur={handleFocusLeave}
+          onChange={(evt) => setTask(evt.target.value)}
+        ></input>
+      </div>
 
+      <div className="d-flex">
+        <a id="updateTask input-elements" className="btn bg-white mx-5">
+          <svg
+            onClick={handleUpdateTask}
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="bi bi-pencil-square text-warining"
+            viewBox="0 0 16 16"
+          >
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+            <path
+              fillRule="evenodd"
+              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+            />
+          </svg>
+          
+        </a>
 
-
-                <div className='d-flex w-75'>
-
-
-                    { taskObject.completed ? 
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-hand-thumbs-up-fill text-success mt-2" viewBox="0 0 16 16">
-                <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-              </svg> 
-              
-              : 
-              
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-hand-thumbs-up-fill text-danger mt-2" viewBox="0 0 16 16">
-                <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-              </svg>
-                
-                }
-                <input type='checkbox' id={`taskDone-${taskObject.id}`} defaultChecked={completed} className='mx-5' onChange={(evt)=>setCompleted(evt.target.value)}></input>
-
-                <input type='text'  id={`taskText-${taskObject.id}`}   defaultValue={task} className={`input-elements  ${focused ? 'focused-element' :''} ${completed ? 'text-success fw-bold ' :'text-dark'} w-75`}  placeholder='default' onFocus={handleFocus} onBlur={handleFocusLeave} onChange={(evt)=>setTask(evt.target.value)}></input>
-
-                </div>
-           
-           <div className='d-flex'>
-            <a id='updateTask input-elements'  className='btn bg-white mx-5'>
-
-            <svg onClick={handleUpdateTask} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square text-warining" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-</svg>
-                
-            </a>
-
-            <a id='deleteTask input-elements'  className='btn bg-light'>
-            
-            
-            <svg onClick={deleteTask} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash3-fill text-danger" viewBox="0 0 16 16">
-  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-</svg>
-            
-            </a>
-            </div>
-            
-            
-        </div>
-    )
-}
+        <a id="deleteTask input-elements" className="btn bg-light">
+          <svg
+            onClick={deleteTask}
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="bi bi-trash3-fill text-danger"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Task;
