6c0576fb7c6f41f3890ad3cf1547c12b10fec404
amaljs <amaljs121@gmail.com>
2023-11-26 21:51:04 +0530

    urls.py comments

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,12 @@ from django.urls import include, path
 from tasks import views
 from rest_framework import routers
 
+#router objects handles the routing 
+#router generate the urls for the viewset 
+#base name to avoid conflicts when have more routers tasks/  ==== tasks-todo
+
 router=routers.DefaultRouter()
-router.register('tasks',views.TaskViewSets,basename='tasks') 
+router.register('tasks',views.TaskViewSets,basename='tasks-todo') 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
